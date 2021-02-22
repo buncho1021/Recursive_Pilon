5fc032e46e8cba4685a44b7c113193e55e915ca3
buncho <78598272+buncho1021@users.noreply.github.com>
2021-02-22 12:17:16 +0900

    Update Recur_Pilon.sh

--- Recur_Pilon.sh
+++ Recur_Pilon.sh
@@ -23,6 +23,6 @@ bowtie2 --end-to-end --very-sensitive --qc-filter -p 28 -x PEtoREF -1 $2 -2 $3 -
 samtools view -@ 28 -bS PEtoREF.sam > PEtoREF.bam &&
 samtools sort -@ 28 PEtoREF.bam > PEtoREF_sorted.bam &&
 samtools index PEtoREF_sorted.bam &&
-java -Xmx500G -jar ~/pilon-1.23.jar --genome ./out.fasta --frags PEtoREF_sorted.bam --outdir ./ --output out --mingap 0  --changes --threads 8
+java -Xmx500G -jar ~/pilon-1.23.jar --genome ./out.fasta --frags PEtoREF_sorted.bam --outdir ./ --output 15pilished_sequences --mingap 0  --changes --threads 8
 rm ./PEtoREF*
 
